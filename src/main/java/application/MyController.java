aa289c1ab09d45f5f96b9aea71f729cee5bb21cf
OanaLuminita <oana.luminita.brezae@gmail.com>
2020-09-27 21:00:24 +0300

    the controller

---
+++ src/main/java/application/MyController.java
@@ -0,0 +1,16 @@
+package application;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value="/sample")
+public class MyController {
+
+	@GetMapping
+	public @ResponseBody MyObject getMyObject() {
+		return new MyObject("Hello from git app in the new repo! 27.09.2020");
+	}
+}
---
+++ src/main/java/application/MyObject.java
@@ -0,0 +1,21 @@
+package application;
+
+public class MyObject {
+	private String message;
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+	public MyObject(String message) {
+		super();
+		this.message = message;
+	}
+	
+	
+
+}
